a7b0a72ccc5e1ce7f20c26048efd3c32dd9343b1
rojan96 <rojanm874@gmail.com>
2020-06-02 17:28:31 -0500

    Added Basic Routing

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import {BusinessBanner} from '../BusinessBanner/BusinessBanner';
+import About from '../About/About';
+import Search from '../Search/Search';
+
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 const App = () => {
   return(
-    <BusinessBanner />
+      <Router>
+        <div>
+          <Switch>
+            <Route path="/" exact component={BusinessBanner}/>
+            <Route path="/About" component={About} />
+            <Route path="/Search" component={Search} />
+          </Switch>
+        </div>
+      </Router>
+
   );
 }
 
--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -2,12 +2,13 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav';
+import {Link} from 'react-router-dom';
 
 const styles = {
     fontSize: 30,
-    opacity:'1'
+    opacity:'1',
+    color: 'Black'
 }
-
 
 export class NavBar extends React.Component {
 
@@ -21,6 +22,7 @@ export class NavBar extends React.Component {
                         height={"200"}
                         width={"200"}
                         alt="Img not found"
+                        style={{backgroundColor: 'Black'}}
                         />
                     </Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
@@ -28,10 +30,18 @@ export class NavBar extends React.Component {
                     <Nav className="mr-auto">
                     </Nav>
                     <Nav>
-                        <Nav.Link href="#memes" className="align-top" style={styles}>What we do</Nav.Link>
-                        <Nav.Link href="#deets" className="align-top" style={styles}>Contact us </Nav.Link>
-                        <Nav.Link href="#deets" className="align-top" style={styles}>Log in</Nav.Link>
-                        <Nav.Link href="#deets" className="align-top" style={styles}>Sign up</Nav.Link>
+                        {/*<Link>*/}
+                            <Nav.Link href="#memes" className="align-top" style={styles}>What we do</Nav.Link>
+                        {/*</Link>*/}
+                        <Link to="/Search">
+                            <Nav.Link href="#1" className="align-top" style={styles}>Find Businesses </Nav.Link>
+                        </Link>
+                        <Link to="/Login">
+                            <Nav.Link href="#2" className="align-top" style={styles}>Log in</Nav.Link>
+                        </Link>
+                        <Link to="/Signup">
+                            <Nav.Link href="#3" className="align-top" style={styles}>Sign up</Nav.Link>
+                        </Link>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
