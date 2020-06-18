c0f17f50e5519c358b5a787a3208724ae88abcfa
rojan96 <rojanm874@gmail.com>
2020-06-18 12:04:28 -0500

    added comments on orders

--- src/Components/Orders/Orders.js
+++ src/Components/Orders/Orders.js
@@ -11,46 +11,46 @@ const divStyles = {
 
 function Orders() {
     const {user} = useContext(AuthContext);
-
-    useEffect(() => {
-        const orderIDs = fetchItems();
-        console.log(fetchEachOrder(orderIDs));
-    }, []);
-
-    const [items, setItems] = useState([]);
-
-    const fetchItems = async () => {
-        const data = await axios.get(
-            `https://cors-anywhere.herokuapp.com/https://hpeopleserver.herokuapp.com/users/${user.id}/orders/`, {
-                auth: {
-                    username: 'c',
-                    password: 'c'
-                }
-            }
-        );
-        return data.data.content;
-    }
-
-    const fetchEachOrder = async (orderIDs) => {
-        let orders = [];
-        for (let orderID in orderIDs){
-            console.log(orderID.id);
-            const orderDataJson = await axios.get(
-                `https://cors-anywhere.herokuapp.com/https://hpeopleserver.herokuapp.com/users/${user.id}/orders/${orderID.id}/orderItems/`, {
-                    auth: {
-                        username: 'c',
-                        password: 'c'
-                    }
-                }
-            );
-            console.log(orderDataJson)
-            orders.push(orderDataJson.content);
-        }
-
-        setItems(orders);
-        return orders;
-    }
-
+    //
+    // useEffect(() => {
+    //     const orderIDs = fetchItems();
+    //     console.log(fetchEachOrder(orderIDs));
+    // }, []);
+    //
+    // const [items, setItems] = useState([]);
+    //
+    // const fetchItems = async () => {
+    //     const data = await axios.get(
+    //         `https://cors-anywhere.herokuapp.com/https://hpeopleserver.herokuapp.com/users/${user.id}/orders/`, {
+    //             auth: {
+    //                 username: 'c',
+    //                 password: 'c'
+    //             }
+    //         }
+    //     );
+    //     return data.data.content;
+    // }
+    //
+    // const fetchEachOrder = async (orderIDs) => {
+    //     let orders = [];
+    //     for (let orderID in orderIDs){
+    //         console.log(orderID.id);
+    //         const orderDataJson = await axios.get(
+    //             `https://cors-anywhere.herokuapp.com/https://hpeopleserver.herokuapp.com/users/${user.id}/orders/${orderID.id}/orderItems/`, {
+    //                 auth: {
+    //                     username: 'c',
+    //                     password: 'c'
+    //                 }
+    //             }
+    //         );
+    //         console.log(orderDataJson)
+    //         orders.push(orderDataJson.content);
+    //     }
+    //
+    //     setItems(orders);
+    //     return orders;
+    // }
+    let items = {};
     try {
         return (
             <div style={divStyles}>
