3a241225497ff104e55afe4d1764c872d0562696
chiragawale <chiragawale777@gmail.com>
2020-07-01 00:34:28 -0500

    Update User and Business

--- src/components/Profile/EditProfile.js
+++ src/components/Profile/EditProfile.js
@@ -1,6 +1,10 @@
 import React, { useState, useContext } from "react";
 import { Form, Button, Col } from "react-bootstrap";
-import { updateBusiness } from "../../util/HPserver";
+import {
+    updateInfo,
+    urlProfileInfo,
+    urlGetBusinessInfo,
+} from "../../util/HPserver";
 import { AuthContext } from "../../context/auth";
 
 export const EditBusiness = ({ business, handleClose, onUpdateBusiness }) => {
@@ -103,12 +107,114 @@ export const EditBusiness = ({ business, handleClose, onUpdateBusiness }) => {
                         bCategories: bCategories,
                     };
                     onUpdateBusiness(businessInfo);
-                    const editBusiness = await updateBusiness(
+                    const editBusiness = await updateInfo(
                         user,
-                        businessInfo
+                        businessInfo,
+                        urlGetBusinessInfo
                     );
                     if (editBusiness === "success") {
                         alert(`Business edited successfully.`);
+                    } else {
+                        alert(`Edit failed.`);
+                    }
+                    handleClose();
+                }}
+            >
+                Submit
+            </Button>
+        </Form>
+    );
+};
+
+export const EditUser = ({ userInfo, handleClose, onUpdateUser }) => {
+    const [preference, setPreference] = useState(userInfo.preference);
+    const [address, setAddress] = useState(userInfo.address);
+    const [phone, setPhone] = useState(userInfo.phone);
+    const [firstName, setFirstName] = useState(userInfo.firstName);
+    const [lastName, setLastName] = useState(userInfo.lastName);
+    const [profilePictureUrl, setProfilePictureUrl] = useState(
+        userInfo.profilePictureUrl
+    );
+    const [email, setEmail] = useState(userInfo.email);
+
+    const { user } = useContext(AuthContext);
+
+    return (
+        <Form style={{ color: "black" }}>
+            <Form.Group controlId="formFirstName">
+                <Form.Label>First Name</Form.Label>
+                <Form.Control
+                    defaultValue={firstName}
+                    onChange={(e) => setFirstName(e.target.value)}
+                />
+            </Form.Group>{" "}
+            <Form.Group controlId="formLastName">
+                <Form.Label>Last Name</Form.Label>
+                <Form.Control
+                    defaultValue={lastName}
+                    onChange={(e) => setLastName(e.target.value)}
+                />
+            </Form.Group>
+            <Form.Group controlId="formAddress">
+                <Form.Label>Address</Form.Label>
+                <Form.Control
+                    defaultValue={address}
+                    onChange={(e) => setAddress(e.target.value)}
+                />
+            </Form.Group>
+            <Form.Row>
+                <Form.Group as={Col} controlId="formPreference">
+                    <Form.Label>Preference</Form.Label>
+                    <Form.Control
+                        defaultValue={preference}
+                        onChange={(e) => setPreference(e.target.value)}
+                    />
+                </Form.Group>
+                <Form.Group as={Col} controlId="formPhone">
+                    <Form.Label>Phone</Form.Label>
+                    <Form.Control
+                        defaultValue={phone}
+                        onChange={(e) => setPhone(e.target.value)}
+                    />
+                </Form.Group>
+            </Form.Row>
+            <Form.Group controlId="formProfilePictureUrl">
+                <Form.Label>Profile Picture Url</Form.Label>
+                <Form.Control
+                    defaultValue={profilePictureUrl}
+                    onChange={(e) => setProfilePictureUrl(e.target.value)}
+                />
+            </Form.Group>
+            <Form.Group controlId="formGridEmail">
+                <Form.Label>Email</Form.Label>
+                <Form.Control
+                    type="email"
+                    defaultValue={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+            </Form.Group>
+            <Button
+                variant="primary"
+                onClick={async () => {
+                    const updatedUserInfo = {
+                        id: userInfo.id,
+                        username: userInfo.username,
+                        email: email,
+                        phone: phone,
+                        address: address,
+                        firstName: firstName,
+                        lastName: lastName,
+                        profilePictureUrl: profilePictureUrl,
+                        preference: preference,
+                    };
+                    onUpdateUser(updatedUserInfo);
+                    const editUser = await updateInfo(
+                        user,
+                        updatedUserInfo,
+                        urlProfileInfo
+                    );
+                    if (editUser === "success") {
+                        alert(`User edited successfully.`);
                     } else {
                         alert(`Edit failed.`);
                     }
